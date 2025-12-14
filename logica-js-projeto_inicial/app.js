11119788240d68ade1e47f1569a71b6e9b9e8ebc
Bia Pinheiro <anabeatrizdp2@gmail.com>
2025-12-14 06:36:19 -0300

    Arquivo pronto. Alert, let e prompt.

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Welcome to the Secret Number game');
+let numeroSecreto = 29;
+prompt('Choose the secret number between 1 and 30'); 
+
+//alert -> Vai exibir uma mensagem (It will display a message)
+//let  -> uma variável que vai guardar uma informação em um espaço de memória. Obs: a variável tem que ter um nome, para podemos especificar
+//prompt -> É similar com alert, porém, possui um input que permite que o usuário insiram um dado, como texto por exemplo. 
+//         (It is similar to an alert, however, it has an input that allows the user to enter data, such as text.)
