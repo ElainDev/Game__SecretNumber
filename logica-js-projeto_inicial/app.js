a1b7ed3fb451677358b5fec2a6a03bba6e8c376d
Bia Pinheiro <anabeatrizdp2@gmail.com>
2025-12-14 15:34:02 -0300

    Apredendo usar While

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,13 +1,30 @@
 alert('Welcome to the Secret Number game');
-let numeroSecreto = 5;
-let chute = prompt('Choose the secret number between 1 and 30'); 
+let secretNumber = 5;
+console.log(secretNumber);
+let kick; 
 
-if(chute == numeroSecreto){ // Se o chute for igualzinho ao Numero Secreto, caso a contição for verdadeira, vai exibir uma mensagem
-    console.log('Congratulations! You have discovered the secret number(5)!'); 
+//enquanto o chute não for igual ao numero secreto
+while (kick != secretNumber) {
+    kick = prompt('Choose the secret number between 1 and 10');
+
+    // Se o kick for igualzinho ao Secret Number, caso a contição for verdadeira, vai exibir uma mensagem. Senão for igual, vai exibir outra mensagem.
+    if (kick == secretNumber){ //Se (True) 
+        alert(`Congratulations! You have discovered the secret number: ${secretNumber}!`); 
+    } else { //senão (false)                                                                
+        if (kick > secretNumber) {
+            alert(`The secret number is less than ${kick}`);
+        } else {
+            alert (`The secret number is greater than ${kick}`)
+        }
+    }
 }
+
+
 
 //alert -> Vai exibir uma mensagem (It will display a message)
 //let  -> uma variável que vai guardar uma informação em um espaço de memória. Obs: a variável tem que ter um nome, para podemos especificar
 //prompt -> É similar com alert, porém, possui um input que permite que o usuário insiram um dado, como texto por exemplo. 
 //         (It is similar to an alert, however, it has an input that allows the user to enter data, such as text.)
-//console.log -> Exibe a mensagem no terminal (ferramenta desenvolvedor)
+//console.log -> Exibe a mensagem no terminal (ferramenta desenvolvedor) 
+//              (Display the message in the terminal)
+//template string -> &{} ou "+" usado serve como uma alternativa ao `-> template de strings já apresentado para realizarmos concatenação de strings/caracteres/valores de varíaveis no JavaScript.
